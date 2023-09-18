ec691d04e4b15c28731d8c2b64962ea1e2103b9b
SnakeSpeak <emagpantay3@gmail.com>
2023-09-17 21:02:55 -0700

    commit 	deleted:    accident_model_v1.pkl 	deleted:    accident_model_v2.pkl 	modified:   injury_prediction.html 	modified:   js/ed-ml-app.js 	new file:   tfjs_model/group1-shard1of1.bin 	new file:   tfjs_model/model.json

--- js/ed-ml-app.js
+++ js/ed-ml-app.js
@@ -1,44 +1,75 @@
-
-function showData() {
-    //parse out api data
-    d3.json("http://127.0.0.1:5000//api/v1.0/CRSS_data").then((data) =>)
-}
-
-function createLightingBarGraph(state) {
-    //parse out api data
-    d3.json("http://127.0.0.1:5000//api/v1.0/lighting_conditions").then((data) => {
-        //filter to only see selected state data
-        let value = data.filter(result => result.state == state)
-        const lightingCounts = {};
-        //increase counts for specific lighting types
-        value.forEach(item => {
-            
-            const lighting = item.lighting_condition;
-            lightingCounts[lighting] = (lightingCounts[lighting] || 0)+1;
-        })
-        //create data for chart
-        const lightingData = 
-            {
-                x: Object.keys(lightingCounts),
-                y: Object.values(lightingCounts),
-                type: 'bar'
-            };
-        //create layout, and slight adjustments for readability
-        const layout = {
-            xaxis: {
-                title: 'Lighting Condition',
-                automargin: true,
-                tickangle: -45
-            },
-            yaxis:{
-                title: 'Count'
-            },
-            width: 450,
-            height: 400
-        };
-        //plot it out
-        Plotly.newPlot('chart',[lightingData],layout);
-    })
-
+async function predict() {
+    model = await tf.loadLayersModel("tfjs_model/model.json");
+            // Get input values from the dropdown elements
+    var carBodySelect = document.getElementById("car_body_type").value;
+    var collisionSelect = document.getElementById("manner_of_collision").value;
+    var dayOfWeek = document.getElementById("day_of_the_week").value
+    var hour = document.getElementById("hour").value
+    var month = document.getElementById("month").value
+    var sex = document.getElementById("sex").value
+    var speed = document.getElementById("speed").value
+    var weather = document.getElementById("weather").value
+    var age = document.getElementById("age").value
     
+    var input = [[
+        0,
+        parseInt(carBodySelect),
+        parseInt(collisionSelect),
+        parseInt(dayOfWeek),
+        0,
+        parseInt(hour),
+        parseInt(month),
+        parseInt(sex),
+        parseInt(speed),
+        parseInt(weather), 
+        parseInt(age)
+    ]]
+    var prediction = model.predict(tf.tensor(input))
+    console.log(prediction.data())
+    var noApparentInjury = (prediction.arraySync()[0][0] *100).toFixed(2) + "%";
+    var possibleInjury = (prediction.arraySync()[0][1] *100).toFixed(2) + "%"
+    var suspectMinInjury = (prediction.arraySync()[0][2] *100).toFixed(2) + "%"
+    var suspectSerInjury = (prediction.arraySync()[0][3] *100).toFixed(2) + "%"
+    var fatalInjury = (prediction.arraySync()[0][4] *100).toFixed(2) + "%"
+    console.log([noApparentInjury,possibleInjury,suspectMinInjury,suspectSerInjury,fatalInjury])
+    displayPrediction();
+
+    var class1Element = document.getElementById("class1");
+    class1Element.textContent = noApparentInjury.toLocaleString();
+
+    var class2Element = document.getElementById("class2");
+    class2Element.textContent = possibleInjury.toLocaleString();
+
+    var class3Element = document.getElementById("class3");
+    class3Element.textContent = suspectMinInjury.toLocaleString();
+
+    var class4Element = document.getElementById("class4");
+    class4Element.textContent = suspectSerInjury.toLocaleString();
+
+    var class5Element = document.getElementById("class5");
+    class5Element.textContent = fatalInjury.toLocaleString();
 }
+
+function displayPrediction() {
+
+    var nAIValue = window.noApparentInjury
+    var class1Element = document.getElementById("class1");
+    class1Element.textContent = nAIValue
+
+    var pIValue = window.possibleInjury
+    var class2Element = document.getElementById("class2");
+    class2Element.textContent = pIValue
+    
+    var sMIValue = window.suspectMinInjury
+    var class3Element = document.getElementById("class3");
+    class3Element.textContent = sMIValue
+
+    var sSIValue = window.suspectSerInjury
+    var class4Element = document.getElementById("class4");
+    class4Element.textContent = sSIValue
+
+    var fIValue = window.fatalInjury
+    var class5Element = document.getElementById("class5");
+    class5Element.textContent = fIValue
+}
+
